1c3d8e4abe8bc94a82d2174a7e46c2431673c75b
brharris06 <brandon.r.harris06@gmail.com>
2022-04-07 10:45:40 -0500

    complete easy fix

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
 
@@ -10,3 +11,6 @@ public class Main {
         }
     }
 }
+
+// javac -d out/ src/*
+
